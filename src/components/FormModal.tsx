c791e9143ce05c46750fc5fcc5be7297616d9759
frako23 <frako23@gmail.com>
2025-03-20 13:18:27 -0400

    feat: restore and integrate StudentForm component with validation in FormModal

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -9,15 +9,15 @@ import React, { useState } from "react";
 const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
   loading: () => <h1>Loading...</h1>,
 });
-// const StudentForm = dynamic(() => import("./forms/StudentForm"), {
-//   loading: () => <h1>Loading...</h1>,
-// });
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
 
 const forms: {
   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-  // student: (type, data) => <StudentForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
   // parent: (type, data) => <TeacherForm type={type} data={data} />,
   // subject: (type, data) => <TeacherForm type={type} data={data} />,
   // class: (type, data) => <TeacherForm type={type} data={data} />,
