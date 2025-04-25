5c423ee2f517056b7b5cea0f8911b544f2dfebee
frako23 <frako23@gmail.com>
2025-04-25 13:18:04 -0400

    feat: update Clerk integration; upgrade @clerk/nextjs to version 5.7.5, add ClerkProvider to layout, and implement sign-in page with error handling

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+
+function Homepage() {
+  return (
+    <div className="h-screen flex item-center justify-center bg-lamaSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1>SchooLama</h1>
+          <h2>Sign in to your account</h2>
+          <Clerk.GlobalError />
+          <Clerk.Field name="identifier">
+            <Clerk.Label>Username</Clerk.Label>
+            <Clerk.Input type="text" required />
+            <Clerk.FieldError />
+          </Clerk.Field>
+          <Clerk.Field name="password">
+            <Clerk.Label>Password</Clerk.Label>
+            <Clerk.Input type="password" required />
+            <Clerk.FieldError />
+          </Clerk.Field>
+          <SignIn.Action submit>Sign In</SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+}
+
+export default Homepage;
